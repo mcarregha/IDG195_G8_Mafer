29c65f5fa2736bc872675a373421541699e75c6f
mcarregha <maria.carregha@cetys.edu.mx>
2022-03-28 20:49:39 -0700

    cambio de look

---
+++ Prueba/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titleText}>Holiwis</Text>
+      <Text> Que emocionante</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#800000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  titleText:{
+    fontSize: 12
+  }
+});
